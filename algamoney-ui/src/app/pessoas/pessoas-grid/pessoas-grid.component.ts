6c8f9d094165310f5fc6f2987d2c47f8d2c197a6
Thiago <titisbackup@gmail.com>
2020-04-08 14:33:46 -0300

    Finalizando a implementação de paginação lazy na pagina pesquisa pessoas e incluindo filtro por nome

--- algamoney-ui/src/app/pessoas/pessoas-grid/pessoas-grid.component.ts
+++ algamoney-ui/src/app/pessoas/pessoas-grid/pessoas-grid.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { LazyLoadEvent } from 'primeng/components/common/api';
+import { PessoaFiltro, PessoaService } from './../pessoa.service';
+import { Component, Input } from '@angular/core';
+
+import { PessoasPesquisaComponent } from './../pessoas-pesquisa/pessoas-pesquisa.component';
 
 @Component({
   selector: 'app-pessoas-grid',
@@ -8,4 +12,13 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PessoasGridComponent {
 
   @Input() pessoas = [];
+  @Input() totalRegistros: PessoasPesquisaComponent
+  @Input() filtro: PessoaFiltro;
+
+  constructor(private pessoaPesquisa: PessoasPesquisaComponent ) { }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pessoaPesquisa.pesquisar(pagina);
+  }
 }
