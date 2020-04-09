7441b7dc96410ede09f2db8731a179bc0e1a7808
Thiago <titisbackup@gmail.com>
2020-04-08 23:21:17 -0300

    Inicio de implementação de exclusão de lançamento

--- algamoney-ui/src/app/lancamentos/lancamento.service.ts
+++ algamoney-ui/src/app/lancamentos/lancamento.service.ts
@@ -54,4 +54,14 @@ export class LancamentoService {
         return resultado;
       })
   }
+
+  excluir(codigo: number): Promise<void> {
+    const headers = new Headers();
+
+    headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    return this.http.delete(`${this.lancamentoURL}/${codigo}`, { headers })
+      .toPromise()
+      .then(() => null)
+  }
 }
--- algamoney-ui/src/app/lancamentos/lancamentos-grid/lancamentos-grid.component.ts
+++ algamoney-ui/src/app/lancamentos/lancamentos-grid/lancamentos-grid.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, ViewChild } from '@angular/core';
 import { LazyLoadEvent } from 'primeng/components/common/api';
 
 import { LancamentosPesquisaComponent } from '../lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { LancamentoFiltro } from './../lancamento.service';
+import { LancamentoFiltro, LancamentoService } from './../lancamento.service';
 
 @Component({
   selector: 'app-lancamentos-grid',
@@ -15,13 +15,26 @@ export class LancamentosGridComponent {
   @Input() lancamentos = [];
   @Input() totalRegistros: LancamentosPesquisaComponent;
   @Input() filtro: LancamentoFiltro;
+  @ViewChild('tabela') grid;
 
   constructor(
-    private lancamentoPesquisa: LancamentosPesquisaComponent
+    private lancamentoPesquisa: LancamentosPesquisaComponent,
+    private lancamentoService: LancamentoService
   ) {}
 
   aoMudarPagina(event: LazyLoadEvent) {
     const pagina = event.first / event.rows
     this.lancamentoPesquisa.pesquisar(pagina);
+  }
+
+  excluir(lancamento: any) {
+    this.lancamentoService.excluir(lancamento.codigo)
+      .then(() => {
+        if (this.grid.first === 0) {
+          this.lancamentoPesquisa.pesquisar();
+        } else {
+          this.grid.first = 0;
+        }
+      });
   }
 }
